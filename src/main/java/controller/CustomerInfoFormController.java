904c87f4ddc2cafb53ceea30f1f00d6e80d8080f
Vikum Kanishka <vikumkanishka14@gmail.com>
2026-01-27 23:38:22 +0530

    Implement customer data insertion in the database from the information form

--- src/main/java/controller/CustomerInfoFormController.java
+++ src/main/java/controller/CustomerInfoFormController.java
@@ -7,6 +7,12 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TextField;
 import model.CustomerDto;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+
 public class CustomerInfoFormController {
 
     @FXML
@@ -66,6 +72,27 @@ public class CustomerInfoFormController {
         String address = txtCusAddress.getText();
         Integer phone = Integer.parseInt(txtCusPhone.getText());
         String regdate = String.valueOf(cusRegDate.getValue());
+
+        CustomerDto customerDto = new CustomerDto(id, firstname, lastname, email, phone, address, city, regdate);
+
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/Thogakade_Shop_Management_System", "root", "200004602360");
+
+            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO customer VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+
+            preparedStatement.setObject(1,id);
+            preparedStatement.setObject(2,firstname);
+            preparedStatement.setObject(3,lastname);
+            preparedStatement.setObject(4,email);
+            preparedStatement.setObject(5,phone);
+            preparedStatement.setObject(6,address);
+            preparedStatement.setObject(7,city);
+            preparedStatement.setObject(8,regdate);
+
+            preparedStatement.execute();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
 
     }
