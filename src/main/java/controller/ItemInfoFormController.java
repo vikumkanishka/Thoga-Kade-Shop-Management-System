11c14456bcca0be8e7c65a16ed989d9bb33a4e00
Vikum Kanishka <vikumkanishka14@gmail.com>
2026-02-08 22:16:33 +0530

    Enhance Item Info form with action handlers and updated UI components

--- src/main/java/controller/ItemInfoFormController.java
+++ src/main/java/controller/ItemInfoFormController.java
@@ -1,4 +1,20 @@
 package controller;
 
+import javafx.event.ActionEvent;
+
 public class ItemInfoFormController {
+    public void btnAddOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnUpdateOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnDeleteOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnClearOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSearchOnAction(ActionEvent actionEvent) {
+    }
 }
