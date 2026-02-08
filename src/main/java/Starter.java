39b2a296444a613086effcab08e131b29e8f8517
Vikum Kanishka <vikumkanishka14@gmail.com>
2026-02-08 21:31:00 +0530

    Add customer data retrieval and observable list for customer information form

--- src/main/java/Starter.java
+++ src/main/java/Starter.java
@@ -12,5 +12,6 @@ public class Starter extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/customer_Info.fxml"))));
+        stage.show();
     }
 }
