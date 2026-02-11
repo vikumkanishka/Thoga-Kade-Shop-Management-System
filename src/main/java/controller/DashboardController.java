ee9b6672b9ab4cbc0043b16c094c1ac50dce814f
Vikum Kanishka <vikumkanishka14@gmail.com>
2026-02-11 23:35:43 +0530

    Add Dashboard.fxml and DashboardController for main application interface

---
+++ src/main/java/controller/DashboardController.java
@@ -0,0 +1,23 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class DashboardController {
+
+    @FXML
+    void btnCustomerOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnItemOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnPlaceOrderOnAction(ActionEvent event) {
+
+    }
+
+}
