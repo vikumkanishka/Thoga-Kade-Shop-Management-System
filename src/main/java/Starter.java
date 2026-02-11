2904211b8d9af613aa68c8a7f54fe71bf859ddfe
Vikum Kanishka <vikumkanishka14@gmail.com>
2026-02-11 23:44:35 +0530

    Add navigation functionality to DashboardController for customer, item, and order views

--- src/main/java/Starter.java
+++ src/main/java/Starter.java
@@ -11,7 +11,7 @@ public class Starter extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/Item_Info.fxml"))));
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/Dashboard.fxml"))));
         stage.show();
     }
 }
--- src/main/java/controller/DashboardController.java
+++ src/main/java/controller/DashboardController.java
@@ -2,22 +2,44 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class DashboardController {
 
+    Stage stage = new Stage();
+
     @FXML
     void btnCustomerOnAction(ActionEvent event) {
-
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/customer_Info.fxml"))));
+            stage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @FXML
     void btnItemOnAction(ActionEvent event) {
-
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/Item_Info.fxml"))));
+            stage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @FXML
     void btnPlaceOrderOnAction(ActionEvent event) {
-
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/order_Info.fxml"))));
+            stage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
